03ef1a7ea248d178237e0b58ac5e338612f9aedb
Oleh Kushka <kushkaoleg@gmail.com>
2024-03-02 21:02:29 +0200

    Add to-do list functionality

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
+const toDoList = new Map();
+let counter = 0;
+
 const addToDo = document.getElementById("addToDo");
+const toDoListContainer = document.getElementById("toDoListContainer");
+let deleteButtons = document.getElementsByClassName("to-do-delete");
+deleteButtons = Array.from(deleteButtons);
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    deleteToDo(button.id);
+  });
+});
 
 addToDo.addEventListener("click", function () {
   const text = document.getElementById("newToDo").value;
+  if (text === "") return;
+  counter++;
+  toDoList.set(counter, text);
+  toDoListContainer.innerHTML += `<div class="to-do">
+  <p class="to-do-text">
+    ${text}
+  </p>
+  <div class="buttons-container">
+    <div class="to-do-delete" id=deleteTodo${counter}><img src="trash-solid.svg" alt="" /></div>
+    <div class="to-do-delete">
+      <img src="circle-check-regular.svg" alt="" />
+    </div>
+  </div>
+</div>`;
+  console.log(deleteButtons);
+  const newButton = document.getElementById(`deleteTodo${counter}`);
+  newButton.addEventListener("click", function () {
+    deleteToDo(newButton.id);
+  });
+  deleteButtons.push(document.getElementById(`deleteTodo${counter}`));
 });
+
+const deleteToDo = (id) => {
+  let numberId = id.match(/\d+/)[0];
+  toDoList.delete(+numberId);
+  toDoListContainer.innerHTML = "";
+  toDoList.forEach((value, key) => {
+    toDoListContainer.innerHTML += `<div class="to-do">
+  <p class="to-do-text">
+    ${value}
+  </p>
+  <div class="buttons-container">
+    <div class="to-do-delete" id=deleteTodo${key}><img src="trash-solid.svg" alt="" /></div>
+    <div class="to-do-delete">
+      <img src="circle-check-regular.svg" alt="" />
+    </div>
+  </div>
+</div>`;
+  });
+};
